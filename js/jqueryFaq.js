d724a59001af4851dfb687cdc7239e10dbed82ad
MacOS <josemmaldonado50@gmail.com>
2021-10-04 10:43:05 -0500

    ajax store

--- js/jqueryFaq.js
+++ js/jqueryFaq.js
@@ -1,14 +1,44 @@
 "use strict";
 
-$(document).ready(function() {
-    $('#highlight-important').click(function(event) {
+$(document).ready(function () {
+    $('#highlight-important').click(function (event) {
         event.preventDefault();
         $('dt').toggleClass('highlighted');
     });
 });
-$(document).ready(function(){
-    $('#invisible-answers').click(function(event){
+$(document).ready(function () {
+    $('#invisible-answers').click(function (event) {
         event.preventDefault();
         $('dd').toggleClass('invisible');
     });
 });
+$(document).ready(function () {
+    $('button').click(function (event) {
+        event.preventDefault();
+        $('ul').each(function () {
+            $(this).children().last.toggleClass('highlighted');
+        });
+    });
+});
+$.ajax("https://hookb.in/Z2rgN2ok3JhK6bmm6bPb",{
+    type:"POST",
+    data:{
+        weekday:"Thorsday",
+        type:"week",
+
+    }
+})
+$.ajax("https://www.themealdb.com/api/json/v1/1/random.php").done(function(data){
+    console.log(data)
+});
+
+
+//
+// $(document).ready(function (){
+//     $('#parks').click(function (event){
+//         event.preventDefault();
+//         $('li').last().each(function (index) {
+//         .
+//         });
+//     });
+//     });
