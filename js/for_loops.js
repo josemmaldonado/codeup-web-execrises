659e449389b58cf4c3b99c689003d634421de77b
MacOS <josemmaldonado50@gmail.com>
2021-10-17 16:45:22 -0500

    :pizza form

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,27 +2,35 @@ console.log("Snoop a loop, bring your green hat.");
 
 
 var number = 0;
+
 function showMultiplicationTable(number) {
     var i = 0;
     do {
-        var multiply = number*i;
-        console.log(" "+number+ " x "+i+" = "+multiply+ " ");
+        var multiply = number * i;
+        console.log(" " + number + " x " + i + " = " + multiply + " ");
         i++;
-    } while (i<=10);
+    } while (i <= 10);
 }
 
+function oddEven(x) {
 
-for (var i =1; i < 10; i++) {
-    for (var i = "A"; i.length < 8; i = i + "A")
-
-
-    console.log(i)
 }
 
+function numberRows(x) {
+    var rows = x;
+    var space = "";
+    for (var i = 1; i <= rows; i++) {
+        for (var j = 1; j <= i; j++) {
+            console.log(j += space);
+        }
 
+    }
+    // console.log(space )
+}
 
+function subtractFive(number){
+    var x = number;
+    for (var i = 100; i >= 5; i - 5)
+        console.log(i)
+}
 
-   // for (var sf = 100; sf >= 5; sf -= 5) {
-    //    console.log(sf)
-  //  }
-//}
